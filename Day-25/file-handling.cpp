4663891596f4c31b5bc7fb4a4808849ac646fc05
Muhammad Farooq <enmuhammadfarooq@gmail.com>
2026-02-12 18:56:44 +0500

    Day-25

---
+++ Day-25/file-handling.cpp
@@ -0,0 +1,17 @@
+// File handling in C++:
+
+// File handling is a crucial aspect of programming that allows us to read from and write to files. In C++, we can use the fstream library to perform file operations. The fstream library provides three main classes: ifstream for reading from files, ofstream for writing to files, and fstream for both reading and writing.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    static const char * originalFile = "original.txt";
+    // rename(originalFile, "renamed.txt");
+    // remove("original.txt");
+    // FILE * file = fopen(originalFile, "w");
+    // fclose(file);    
+
+    return 0;
+}
